e30ebcc8e3db9ec05a14595b8ba43dedac0b14a0
Ethan Ky <132713901+eky2429@users.noreply.github.com>
2024-02-17 16:53:35 +0000

    Updated code, again

--- app.rb
+++ app.rb
@@ -13,19 +13,19 @@ get("/umbrella") do
   erb(:umbrella)
 end
 
-get("/process_umbrella") do
+post("/process_umbrella") do
   @user_location = params.fetch("user_location")
+  input = @user_location.gsub(" ", "+")
 
   GMAPS_KEY = ENV.fetch("GMAPS_KEY")
   PIRATE_WEATHER_KEY = ENV.fetch("PIRATE_WEATHER_KEY")
 
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + input + "&key=" + GMAPS_KEY
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{input}&key=#{GMAPS_KEY}"
   gmaps_response = HTTP.get(gmaps_url)
   gmaps_info = JSON.parse(gmaps_response)
-  @gmaps_loc = gmaps.info.dig("results", 0, "geometry", "location")
-  @lat = @gmaps_loc.fetch("lat")
-  @lon = @gmaps_loc.fetch("lng")
-
+  gmaps_loc = gmaps.info.dig("results", 0, "geometry", "location")
+  @lat = gmaps_loc.fetch("lat")
+  @lon = gmaps_loc.fetch("lng")
 
   pirate_weather_info = JSON.parse(HTTP.get("https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/#{latitude},#{longitude}"))
   
