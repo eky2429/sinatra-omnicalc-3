db32870e884c4bbf79113a406c20128c1c640148
Ethan Ky <132713901+eky2429@users.noreply.github.com>
2024-02-17 16:57:44 +0000

    Fixed errors in code

--- app.rb
+++ app.rb
@@ -23,13 +23,10 @@ post("/process_umbrella") do
   gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{input}&key=#{GMAPS_KEY}"
   gmaps_response = HTTP.get(gmaps_url)
   gmaps_info = JSON.parse(gmaps_response)
-  gmaps_loc = gmaps.info.dig("results", 0, "geometry", "location")
+  gmaps_loc = gmaps_info.dig("results", 0, "geometry", "location")
   @lat = gmaps_loc.fetch("lat")
   @lon = gmaps_loc.fetch("lng")
 
-  pirate_weather_info = JSON.parse(HTTP.get("https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/#{latitude},#{longitude}"))
-  
-latitude = google_maps_info.fetch("lat")
-  HTTP.get()
+  #pirate_weather_info = JSON.parse(HTTP.get("https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/#{latitude},#{longitude}"))
   erb(:umbrella_results)
 end
