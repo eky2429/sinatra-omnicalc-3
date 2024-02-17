109c7ce5fff733e4c29570cecea33d84e71bf782
Ethan Ky <132713901+eky2429@users.noreply.github.com>
2024-02-17 16:44:58 +0000

    Added More to Umbrella

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
@@ -14,5 +15,21 @@ end
 
 get("/process_umbrella") do
   @user_location = params.fetch("user_location")
+
+  GMAPS_KEY = ENV.fetch("GMAPS_KEY")
+  PIRATE_WEATHER_KEY = ENV.fetch("PIRATE_WEATHER_KEY")
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + input + "&key=" + GMAPS_KEY
+  gmaps_response = HTTP.get(gmaps_url)
+  gmaps_info = JSON.parse(gmaps_response)
+  @gmaps_loc = gmaps.info.dig("results", 0, "geometry", "location")
+  @lat = @gmaps_loc.fetch("lat")
+  @lon = @gmaps_loc.fetch("lng")
+
+
+  pirate_weather_info = JSON.parse(HTTP.get("https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/#{latitude},#{longitude}"))
+  
+latitude = google_maps_info.fetch("lat")
+  HTTP.get()
   erb(:umbrella_results)
 end
